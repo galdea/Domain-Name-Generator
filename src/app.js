d501f27982409e418cdb5bab7cd7daeae29aa7da
Galdea <gabriel.aldea@gmail.com>
2022-12-01 13:18:33 +0000

    version acabada

--- src/app.js
+++ src/app.js
@@ -9,3 +9,22 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let address = [".com", ".cl", ".io"];
+
+let excuse = [];
+
+for (const value of pronoun) {
+  let randomValue1 = adj[Math.floor(Math.random() * adj.length)];
+  for (let randomValue1 of adj) {
+    let randomValue2 = noun[Math.floor(Math.random() * noun.length)];
+    for (let randomValue2 of noun) {
+      let randomValue3 = address[Math.floor(Math.random() * address.length)];
+      let frase = "www." + value + randomValue1 + randomValue2 + randomValue3;
+      console.log(frase);
+    }
+  }
+}
